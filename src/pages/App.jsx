e1d0623acfdd4b790c1566e72ddb0e24e66a9de9
Irfan Marzuki <irfnmzk@gmail.com>
2021-05-26 22:44:53 +0700

    add base navbar

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,15 +3,19 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MainPage from "./Main/Main";
 import NotFound from "../components/NotFound/NotFound";
+import Navigation from "../components/Navigation/Navigation";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={MainPage} />
-        <Route exact path="/face" component={MainPage} />
-        <Route path="*" component={NotFound} />
-      </Switch>
+      <Navigation />
+      <div className="bg-violet h-screen pt-14">
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/face" component={MainPage} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </div>
     </Router>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,27 @@ module.exports = {
   mode: "jit",
   purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
   // specify other options here
+
+  theme: {
+    extend: {
+      fontSize: {
+        xxs: ".65rem",
+      },
+      fontFamily: {
+        pixelated: ['"Press Start 2P"', "cursive"],
+      },
+      colors: {
+        superiority: "var(--superiority)",
+        corn: "var(--corn)",
+        grey: "var(--grey)",
+        green: "var(--green)",
+        mandarin: "var(--mandarin)",
+        superiority: "var(--superiority)",
+        violet: "var(--violet)",
+      },
+      boxShadow: {
+        superiority: "4px 5px var(--superiority)",
+      },
+    },
+  },
 };
