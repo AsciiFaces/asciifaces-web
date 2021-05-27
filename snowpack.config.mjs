0c02f0704ff3bc8e9871ce8ac8d093cdceb20e25
Irfan Marzuki <irfnmzk@gmail.com>
2021-05-27 19:21:46 +0700

    using snowpack esbuild

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,28 +1,27 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
-  mount: {
-    public: "/",
-    src: "/dist",
-  },
-  plugins: [
-    "@snowpack/plugin-postcss",
-    ["@snowpack/plugin-webpack", { sourceMap: false }],
-  ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    { match: "routes", src: ".*", dest: "/index.html" },
-  ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    bundle: true,
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    tailwindConfig: "./tailwind.config.js",
-  },
-  buildOptions: {
-    /* ... */
-  },
+    mount: {
+        public: '/',
+        src: '/dist'
+    },
+    plugins: ['@snowpack/plugin-postcss'],
+    routes: [
+        /* Enable an SPA Fallback in development: */
+        { match: 'routes', src: '.*', dest: '/index.html' }
+    ],
+    optimize: {
+        /* Example: Bundle your final build: */
+        bundle: true,
+        minify: true,
+        target: 'es2018'
+    },
+    packageOptions: {
+        /* ... */
+    },
+    devOptions: {
+        tailwindConfig: './tailwind.config.js'
+    },
+    buildOptions: {
+        /* ... */
+    }
 };
